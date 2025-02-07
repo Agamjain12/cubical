54ed5dd7222fbed03c80c06b14bb2a34494a04b7
Abhishek Jha <jabhishek10142@gmail.com>
2025-02-07 20:21:36 +0530

    IO server change

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -7,11 +7,7 @@ let io;
 export const initializeSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: [
-        "http://localhost:5173",
-        "https://cubical-bw9p.onrender.com",
-        "https://cubicals-agam.netlify.app/",
-      ],
+      origin: "*",
       methods: ["GET", "POST"],
       credentials: true,
     },
